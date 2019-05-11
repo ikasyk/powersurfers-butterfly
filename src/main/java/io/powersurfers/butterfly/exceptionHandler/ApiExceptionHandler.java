4cb6fc24576e85df3b60aa07b435e58fb8d70d7d
Michael Rudyy <mcrudyy@gmail.com>
2019-05-11 20:54:58 +0300

    Add Api Exception Handlers

--- src/main/java/io/powersurfers/butterfly/exceptionHandler/ApiExceptionHandler.java
+++ src/main/java/io/powersurfers/butterfly/exceptionHandler/ApiExceptionHandler.java
@@ -1,5 +1,7 @@
 package io.powersurfers.butterfly.exceptionHandler;
 
+import io.powersurfers.butterfly.exception.NoSuchQuestException;
+import io.powersurfers.butterfly.exception.NoSuchQuestStageException;
 import io.powersurfers.butterfly.exception.NoSuchUserException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,8 +15,18 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(NoSuchUserException.class)
-    protected ResponseEntity<ApiException> handleThereIsNoSuchUserException() {
+    protected ResponseEntity<ApiException> handleThereNoSuchUserException() {
         return new ResponseEntity<>(new ApiException("There is no such user"), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(NoSuchQuestException.class)
+    protected ResponseEntity<ApiException> handleThereNoSuchQuestException() {
+        return new ResponseEntity<>(new ApiException("There is no such quest"), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(NoSuchQuestStageException.class)
+    protected ResponseEntity<ApiException> handleThereNoSuchQuestStageException() {
+        return new ResponseEntity<>(new ApiException("There is no such quest stage"), HttpStatus.NOT_FOUND);
     }
 
     @Data
