347e9b85b743f746dcf932ad2352fac0309cd10b
ikasyk <kasyk3@gmail.com>
2019-05-11 21:02:37 +0300

    CORS fix

--- src/main/java/io/powersurfers/butterfly/controller/QuestController.java
+++ src/main/java/io/powersurfers/butterfly/controller/QuestController.java
@@ -3,39 +3,33 @@ package io.powersurfers.butterfly.controller;
 import com.fasterxml.jackson.annotation.JsonView;
 import io.powersurfers.butterfly.dao.QuestRepository;
 import io.powersurfers.butterfly.dao.jsonview.Views;
-import io.powersurfers.butterfly.exception.NoSuchQuestException;
 import io.powersurfers.butterfly.model.Quest;
 import io.powersurfers.butterfly.service.QuestService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/quests")
 public class QuestController {
 
     @Autowired
-    private QuestRepository questsRepository;
-
-    @Autowired
     QuestService questService;
+    @Autowired
+    private QuestRepository questsRepository;
 
     @GetMapping("/")
     @JsonView(Views.QuestField.class)
     public List<Quest> getAll() {
         return questService.getAll();
-//        return Arrays.asList(
-//                new Quest(1, "Ancient Kiev", "Here you can learn about ancient city and history", null),
-//                new Quest(2, "Mistery places", "Learn about mistery places", null)
-//        );
     }
 
     @GetMapping("/{id}")
+    @JsonView(Views.QuestDetailField.class)
     public Quest getQuestById(@PathVariable("id") Integer id) {
         return questService.getQuestById(id);
     }
